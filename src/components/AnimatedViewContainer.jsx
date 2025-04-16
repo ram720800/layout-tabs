c003e9abdbb9b8bef3d7ea20ca124f670d8226bf
ram720800 <code72hub@gmail.com>
2025-04-17 00:49:56 +0530

    fixed clutter while transition

--- src/components/AnimatedViewContainer.jsx
+++ src/components/AnimatedViewContainer.jsx
@@ -9,7 +9,7 @@ const AnimatedViewContainer = ({ viewMode, children }) => {
             key={viewMode}
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.3, ease: "easeInOut" }}
+            transition={{ type: "spring", stiffness: 300, damping: 30 }}
           >
             {children}
           </motion.div>
